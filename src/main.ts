1efdff149c9b17c2fc38f176db5b421758af9c66
Ariel Alejandro Wagner <arielwagner.gcba@gmail.com>
2025-01-18 00:13:18 -0300

    Añado archivo .env y funcionalidades más Sequelize y proveedor de PostgreSQL más sus configuraciones

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT_SERVER ?? 9999, () => {
+    console.log(`===> Servidor corriendo en el Puerto: ${process.env.PORT_SERVER} <===`);
+  });
 }
 bootstrap();
